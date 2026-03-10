471f0562dc7cf20d99b6ca88e3b610ff15ea769d
bd1ogo <barrosof.diogo@gmail.com>
2026-03-10 19:07:56 -0300

    abrindo a base de dados pela tabela .csv, cadastrando produtos em sequencia a partir da informação de linha e coluna

--- projetoAutomação/codigo.py
+++ projetoAutomação/codigo.py
@@ -5,9 +5,10 @@
 # Passo 5: Repetir o passo 4 até acabar a lista de produtos
 import pyautogui
 import time
+import pandas
 
 #garante segurança para não acumular processos
-pyautogui.PAUSE = 1
+pyautogui.PAUSE = 2
 link = "https://dlp.hashtagtreinamentos.com/python/intensivao/login"
 
 #Entrando no Sistema pelo navegador
@@ -30,3 +31,48 @@ pyautogui.write('senhadificil123')
 #logar
 pyautogui.press('tab')
 pyautogui.press('enter')
+
+#pausa maior para o site poder carregar com segurança
+time.sleep(3)
+#Abrindo a base de dados (importar o arquivo .csv)
+tabela = pandas.read_csv('produtos.csv')
+
+for linha in tabela.index:
+    #Cadastrar produto
+    #campo do código
+    pyautogui.click(x=806, y=316) 
+    codigo = str(tabela.loc[linha, 'codigo'])
+    pyautogui.write(codigo)
+    pyautogui.press('tab')
+    #campo da marca
+    marca = str(tabela.loc[linha, 'marca'])
+    pyautogui.write(marca)
+    pyautogui.press('tab')
+    #campo tipo
+    tipo = str(tabela.loc[linha, 'tipo'])
+    pyautogui.write(tipo)
+    pyautogui.press('tab')
+    #campo do categoria
+    categoria = str(tabela.loc[linha, 'categoria'])
+    pyautogui.write(categoria)
+    pyautogui.press('tab')
+    #campo do preço
+    preco = str(tabela.loc[linha, 'preco_unitario'])
+    pyautogui.write(preco)
+    pyautogui.press('tab')
+    #campo de custo
+    custo = str(tabela.loc[linha, 'custo'])
+    pyautogui.write(custo)
+    pyautogui.press('tab')
+    #campo de obs
+    obs = str(tabela.loc[linha, 'obs'])
+    if obs != 'nan':  
+        pyautogui.write(obs)
+    pyautogui.press('tab') #passa para o campo de enviar
+
+    #cadastra o produto
+    pyautogui.press('enter')
+    #voltar para o inicio para cadastrar o próximo produto
+    pyautogui.scroll(5000)
+
+    
